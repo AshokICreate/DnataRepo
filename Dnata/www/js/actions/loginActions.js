5871bd044b477fa9148c271bab20881c08123b7c
Phani Sunkara <phani.sunkara@metricstream.com>
2016-03-16 12:08:33 +0530

    added error message when login failed and also added function to create form

--- Dnata/www/js/actions/loginActions.js
+++ Dnata/www/js/actions/loginActions.js
@@ -2,10 +2,10 @@ define (function (require) {
     var appDispatcher = require ("util/appDispatcher");
     var constants = require ("constants/loginConstants");
     var loginActions = {
-        createLogin: function (text) {
+        doLogin: function (user) {
           appDispatcher.dispatch ({
             actionType: constants.Login_Auth,
-            text: text
+            user: user
           });
         }
     };
