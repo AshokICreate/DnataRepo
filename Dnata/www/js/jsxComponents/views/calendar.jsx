4421013b42fec18bffd780abf021da2d087510a8
Phani Sunkara <phani.sunkara@metricstream.com>
2016-05-12 13:18:36 +0530

    fixed issue with future date

--- Dnata/www/js/jsxComponents/views/calendar.jsx
+++ Dnata/www/js/jsxComponents/views/calendar.jsx
@@ -15,7 +15,7 @@ define(function(require){
 
   getInitialState: function(){
 
-    return ({date:this.props.defaultdate,time:this.props.defaulttime});
+    return ({date:this.props.defaultdate,time:this.props.defaulttime,isVisible:true});
   },
 
   componentDidMount: function () {
@@ -29,24 +29,30 @@ define(function(require){
   },
   validateDateAndTime: function(date,time){
     var currentDate = Moment().format("YYYY-MM-DD HH:mm");
-    if((Moment(currentDate).isAfter(date+" "+time)))
+    if(date != "" && time != "" && Moment(currentDate).isAfter(date+" "+time))
     {
         var dateValue = Moment(date,"YYYY-MM-DD").format("M/DD/YYYY");
         var timeValue = Moment(time, 'HH:mm').format('HH:mm:ss');
         this.props.onSave(this.props.id,dateValue+" "+timeValue);
-        this.setState({date:date,time:time});
+        this.setState({date:date,time:time,isVisible:true});
 
     }else {
         var msgButton = [{"title":"ok"}];
-        NavigationActions.presentPopup(<Msg msgLabel={"invalid_date"} buttons={msgButton} onMsgClick={this._onCancel}/>);
-
         //Not resetting to current date
         var dateValue = Moment().format("YYYY-MM-DD");
         var timeValue = Moment().format("HH:mm");
-        this.setState({date:dateValue,time:timeValue});
+        this.setState({date:dateValue,time:timeValue,isVisible:false});
 
         var dateAndTime =  Moment().format("M/DD/YYYY HH:mm:ss");
         this.props.onSave(this.props.id,dateAndTime);
+
+        var msgLabel = "invalid_date"
+        if(date === "" || time === "")
+        {
+            msgLabel = "future_date"
+        }
+        NavigationActions.presentPopup(<Msg msgLabel={msgLabel} buttons={msgButton} onMsgClick={this._onCancel}/>);
+
     }
 
   },
@@ -62,16 +68,26 @@ define(function(require){
 
  _onCancel: function(){
     NavigationActions.removePopup();
+    this.setState({date:this.state.date,time:this.state.time,isVisible:true});
  },
   render: function() {
     var name = this.props.name;
     var className = "cfield";
+    var time = this.state.time;
+    var date = this.state.date
 
+    var keyDate = "date";
+    var keyTime = "time"
+    if(!this.state.isVisible)
+    {
+        keyDate = "datehide"
+        keyTime = "timehide"
+    }
     return(
       <div className="inputBox">
         <TextLabel name={this.props.name} isRequired={this.props.isRequired}/>
-        <input type="date" className={className+" date"+" icon-Calender"} id="cdate" onChange={this._handleDate} defaultValue={this.state.date}/>
-        <input type="time" className={className+" time"+" icon-Clock-01"} id="ctime" onChange={this._handleTime} defaultValue={this.state.time}/>
+        <input type="date"  key= {keyDate} className={className+" date"+" icon-Calender"} id="cdate" onChange={this._handleDate} defaultValue={date}  />
+        <input type="time" key={keyTime} className={className+" time"+" icon-Clock-01"} id="ctime" onChange={this._handleTime} defaultValue={time} />
       </div>
       );
     }
